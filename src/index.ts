701c69ee5e58723b32621273e9e724b2254fd445
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-11-26 22:17:09 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Vector3 } from '@dcl/sdk/math'
+import { ColliderLayer, GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
+
+
+export function main() {
+
+  const tree = engine.addEntity()
+  Transform.create(tree, {
+    position: Vector3.create(48, 0, 48)
+  })
+  GltfContainer.create(tree, {
+    src: 'assets/tree.glb',
+    visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
+  })
+}
