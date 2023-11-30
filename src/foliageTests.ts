972a1c1377e5d018046e83701aaf58f81832a137
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-11-30 22:30:16 +0100

    garden wip

--- src/foliageTests.ts
+++ src/foliageTests.ts
@@ -14,34 +14,4 @@ export function addAssets() {
     })
 
 
-    const leaves = engine.addEntity()
-    Transform.create(leaves, {
-        position: Vector3.create(12, 5, 12),
-        scale: Vector3.create(5, 5, 5)
-    })
-    GltfContainer.create(leaves, {
-        src: 'assets/leaves6.glb',
-        visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
-    })
-
-    const leaves2 = engine.addEntity()
-    Transform.create(leaves2, {
-        position: Vector3.create(50, 5, 15),
-        scale: Vector3.create(5, 5, 5)
-    })
-    GltfContainer.create(leaves2, {
-        src: 'assets/leaves7.glb',
-        visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
-    })
-
-
-    const leaves5 = engine.addEntity()
-    Transform.create(leaves5, {
-        position: Vector3.create(38, 5, 12),
-        scale: Vector3.create(10, 10, 10)
-    })
-    GltfContainer.create(leaves5, {
-        src: 'assets/leaves5.glb',
-        visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
-    })
 }
