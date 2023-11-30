18fce22d86185dfd789f10bf1c282001031fc3f2
Kevin Sy <kevin.sy@msn.com>
2023-11-30 14:23:34 -0500

    Add pixel selector

--- src/classes/pixel.ts
+++ src/classes/pixel.ts
@@ -1,11 +1,12 @@
-import { Entity, InputAction, Material, MeshCollider, MeshRenderer, Transform, TransformType, engine, pointerEventsSystem } from "@dcl/sdk/ecs"
-import { Color4 } from "@dcl/sdk/math"
+import { Entity, InputAction, Material, MeshCollider, MeshRenderer, PointerEventType, PointerEvents, Transform, TransformType, engine, pointerEventsSystem } from "@dcl/sdk/ecs"
+import { Color4, Vector3 } from "@dcl/sdk/math"
 import { Vector2 } from "~system/EngineApi"
 
 export class Pixel {
     entity: Entity
     color: Color4 = Color4.White()
     index: Vector2 = { x: -1, y: -1 }
+    selected: boolean = false
 
     constructor(transform: TransformType, index: Vector2) {
         this.entity = engine.addEntity()
@@ -14,6 +15,25 @@ export class Pixel {
         Transform.create(this.entity, transform)
         this.index = index
         this.paint()
+
+        PointerEvents.createOrReplace(this.entity, {
+            pointerEvents: [
+                {
+                    eventType: PointerEventType.PET_HOVER_ENTER,
+                    eventInfo: {
+                        button: InputAction.IA_POINTER,
+                        showFeedback: true,
+                    }
+                },
+                {
+                    eventType: PointerEventType.PET_HOVER_LEAVE,
+                    eventInfo: {
+                        button: InputAction.IA_POINTER,
+                        showFeedback: true,
+                    }
+                }
+            ]
+        })
 
         pointerEventsSystem.onPointerDown({
             entity: this.entity,
@@ -32,7 +52,22 @@ export class Pixel {
     paint(c: Color4 = Color4.White()) {
         this.color = c
         Material.setPbrMaterial(this.entity, {
-            albedoColor: c
+            albedoColor: c,
+            metallic: 0.25
         })
+    }
+
+    select() {
+        if (this.selected) return
+        this.selected = true
+        let t = Transform.getMutable(this.entity)
+        t.position = Vector3.create(t.position.x, t.position.y, t.position.z - t.scale.z * .4)
+    }
+
+    deselect() {
+        if (!this.selected) return
+        this.selected = false
+        let t = Transform.getMutable(this.entity)
+        t.position = Vector3.create(t.position.x, t.position.y, t.position.z + t.scale.z * .4)
     }
 }
--- src/classes/pixelCanvas.ts
+++ src/classes/pixelCanvas.ts
@@ -1,14 +1,18 @@
 import { Quaternion, Vector3 } from "@dcl/sdk/math"
 import { Pixel } from "./pixel"
-import { Entity, engine, TransformType, Transform } from "@dcl/sdk/ecs"
+import { Entity, engine, TransformType, Transform, PointerEventsResult, PointerEventType } from "@dcl/sdk/ecs"
 
 export class PixelCanvas {
-
     root: Entity
     pixels: Pixel[] = []
+    cols: number
+    rows: number
+    lastHoverOn: any
 
     constructor(parent: TransformType, cols: number, rows: number, scale: number = 0.5) {
         this.root = engine.addEntity()
+        this.cols = cols
+        this.rows = rows
         Transform.create(this.root, parent)
         for (let y = 0; y < rows; y++) {
             const row: any = []
@@ -23,5 +27,50 @@ export class PixelCanvas {
             }
             this.pixels.push(row)
         }
+        engine.addSystem(this.PointerReadingSystem.bind(this))
+    }
+
+    PointerReadingSystem() {
+        const c = engine.getEntitiesWith(PointerEventsResult)
+        for (const [entity] of c) {
+            const result = PointerEventsResult.get(entity)
+            result.forEach(element => {
+                if (element.hit) {
+                    if (element.state == PointerEventType.PET_HOVER_ENTER) {
+                        // select new pixel
+                        if ((this.lastHoverOn && this.lastHoverOn.hit.entityId != element.hit.entityId) || !this.lastHoverOn) {
+                            let p: any = this.findPixelById(element.hit.entityId!)
+                            if (p) {
+                                p.select()
+                                this.lastHoverOn = element
+                            }
+                        }
+                    }
+                    else if (element.state == PointerEventType.PET_HOVER_LEAVE) {
+                        // unselect last pixel
+                        if (this.lastHoverOn && this.lastHoverOn.hit.entityId == element.hit.entityId) {
+                            let p: any = this.findPixelById(element.hit.entityId!)
+                            if (p) {
+                                p.deselect()
+                                this.lastHoverOn = null
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    findPixelById(id: number) {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                const temp: any = this.pixels[x]
+                const p: Pixel = temp[y]
+                if (p.entity.valueOf() == id) {
+                    return p
+                }
+            }
+        }
+        return null
     }
 }
