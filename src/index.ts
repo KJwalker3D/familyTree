30c8ee5e94ab322a5306501ec5297fc5ea8834b6
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-12-01 23:32:54 +0100

    wishing well + garden assets

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { PIXEL_CANVAS_COLS, PIXEL_CANVAS_ROWS } from './utils'
 import { setupUi } from './ui'
 import { addNPCs } from './npcs'
 import { Seed } from './classes/seed'
+import { GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
 
 
 export function main() {
@@ -21,4 +22,26 @@ export function main() {
   for (let i = 0; i < 20; i++) {
     new Seed({ position: Vector3.create(4 + (i * 2), 0, 4), rotation: Quaternion.fromEulerDegrees(i * 45, i * 45, i * 30), scale: Vector3.One() })
   }
+
+  // collect to water the seeds
+  const wateringCan = engine.addEntity()
+  GltfContainer.create(wateringCan, {
+    src: 'assets/wateringCan.glb'
+  })
+  Transform.create(wateringCan, {
+    position: Vector3.create(66.8, 30.05, 67),
+    scale: Vector3.create(1, 1, 1),
+    rotation: Quaternion.fromEulerDegrees(0, 0, 0)
+  })
+
+  // fill the watering can with water for the seeds
+  const waterTrough = engine.addEntity()
+  GltfContainer.create(waterTrough, {
+    src: 'assets/waterTrough.glb'
+  })
+  Transform.create(waterTrough, {
+    position: Vector3.create(48, 0, 48),
+    scale: Vector3.create(1, 1, 1),
+    rotation: Quaternion.fromEulerDegrees(0, 0, 0)
+  })
 }
