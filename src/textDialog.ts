966bcf7f48bf50026c5773375b24b3d672f2d55d
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-12-09 23:49:10 +0100

    NPC dialogue

---
+++ src/textDialog.ts
@@ -0,0 +1,43 @@
+/**
+*
+* 
+*
+*
+* NPC Dialogue 
+*
+Intro: 
+Hey there, adventurer! Welcome to our End-of-Year Bash in the heart of Decentraland. I'm Tala, your guide for this epic journey. While you're exploring, you'll stumble upon our wild party zone, the chill garden, the wishing well, Memory Lane, and the ultra-cool Digital Nomad’s Oasis. Ready to dive into a quest filled with nostalgia and celebration?
+
+Go to treehouse 1 (dance)
+Time to hit the dance floor, my friend! Our parties are legendary, setting the vibe for our gatherings. But there's more to our hangouts, as you'll soon find in the upcoming tree houses. Your first task: find the party area, and show me your sickest dance moves! There’s a dance-meter in the party area that will fill up as you dance, I’ll join you when it’s full. Let the celebrations kick off!
+
+Complete mission in treehouse 1 (NPC appears dancing beside you)
+Damn, those moves were fire! Here's a little something for your efforts. Now, follow me to the next part of our quest! (give wearable)
+
+Go to treehouse 2 (garden)
+Next stop, our secret garden, where ideas take root and bloom. It’s a chill spot where we plant the seeds of growth. Your mission is to plant some seeds of your own! You’ll need to find the seeds first though, plant them in the middle here and water ‘em to watch the magic unfold.
+
+Complete mission in treehouse 2
+Your plants are thriving! From tiny seeds to this beauty – impressive, right? Ready for the next chapter? (Yes/No prompt)
+
+Go to treehouse 3: (wishing well)
+As dreamers, we've got a Community Wishing Well. Collect the scrolls, jot down your wish, and toss it in the well. Who knows, maybe others are wishing for the same rad stuff!
+
+Complete mission in treehouse 3
+May your wish vibe with the community dreams. Take this token as a reminder. Ready for the next adventure? (Yes/No prompt) (give emote)
+
+Go to treehouse 4: (memory lane)
+Memories shape our journey, so we made Memory Lane to share some moments from 2023. There are so many more to add though, we could fill the whole tree! Anyway, hit up Memory Lane, answer questions about the past year, and you're almost at the Digital Nomad’s Oasis, where the real party's at!
+
+Complete mission in treehouse 4
+Amazing! Our memories make us stronger. Ready for the final chapter? (Yes/No prompt)
+
+Go to treehouse 5: (digital nomad oasis)
+The Digital Nomad's Oasis – a haven for creativity. But first, a mission. Ready for the last task? Your job is to find a lost player from our quest and bring them here. In return, I’ll hook you both up with magic tokens for the special place.
+
+
+Complete mission in treehouse 5
+(NPC appears at the entrance of the digital nomad oasis)
+You nailed it! The oasis is waiting. When you dive in, go ahead and add your creative touch to our virtual gallery. Take this POAP token – your key to the digital nomad life. Ready to create your pixel masterpiece? (Yes/No prompt)
+*/
+
