d63f09a75a1fdde0dbc956e245636fcc4c0e4221
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-12-10 14:50:40 +0100

    plant animation

--- src/foliageTests.ts
+++ src/foliageTests.ts
@@ -1,17 +1,33 @@
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
-import { ColliderLayer, GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
+import { Animator, ColliderLayer, GltfContainer, Transform, engine } from '@dcl/sdk/ecs'
 
 export function addAssets() {
-
+    let position = Vector3.create(48, 0, 48)
 
     const tree = engine.addEntity()
     Transform.create(tree, {
-        position: Vector3.create(48, 0, 48),
+        position: position,
         scale: Vector3.create(1, 1, 1)
     })
     GltfContainer.create(tree, {
         src: 'assets/tree.glb',
         visibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS
+    })
+
+    const plantAnimation = engine.addEntity();
+    Transform.create(plantAnimation, {
+        position: position
+    })
+    GltfContainer.create(plantAnimation, {
+        src: 'assets/garden/plantAnimation.glb'
+    })
+    Animator.create(plantAnimation, {
+        states: [{
+            clip: 'play',
+            playing: true,
+            loop: true, //looping for testing purposes
+        }
+        ]
     })
 
 
