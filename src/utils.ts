59aa400efb8e817c3bcb1331ac7bafc3b68edef0
Kevin Sy <kevin.sy@msn.com>
2023-12-15 19:43:32 -0500

    Remove unused

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,5 @@
 import { Color4 } from "@dcl/sdk/math"
 
-export function getRandomHexColor(): string {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 export const THEME = {
   COLOR: Color4.create(1, 1, 191 / 255, 1)
 }
