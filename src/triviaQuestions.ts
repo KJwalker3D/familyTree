73f8ee648740689e09de91e24eecac9887141f2d
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2023-12-17 22:04:56 +0100

    trivia false answers, textures wip

--- src/triviaQuestions.ts
+++ src/triviaQuestions.ts
@@ -4,29 +4,38 @@
  * 
 Who was the first official supermodel of the metaverse? 
 Tangpoko https://decentraland.org/blog/announcements/the-ultimate-guide-to-mvfw23-in-decentraland 
+Incorrect: Canessa, Shibu
 
 What was the theme of Metaverse Fashion Week 2023? 
 Future Heritage 
+Incorrect: Rebirth and Regeneration, Lucid Dreams
 
 What was the theme of Metaverse Art Week 2023 in Decentraland? 
 The metaverse is dead. Long live the metaverse. 
+Incorrect: Low Poly, High Art; Creativity in the Age of Decentralization
 https://decentraland.org/blog/announcements/metaverse-art-week-2023-the-metaverse-is-dead-long-live-the-metaverse 
 
 What was the main festival stage for Decentraland Music Festival 2023? 
 TRU Band Room Stage https://decentraland.org/blog/announcements/the-ultimate-guide-to-decentraland-music-festival-23 
+Incorrect: Bufalo's Saloon, SugarClub
 
 In what month of 2023 did the AI World Fair take place in Decentraland? 
 October https://decentraland.org/blog/announcements/the-ultimate-guide-to-the-ai-world-fair
+Incorrect: September, March
 
 What new wearable category was introduced in 2023 thanks to community members like Nikki and Doki and the Foundation?
 Hands / Handwear https://decentraland.org/blog/announcements/a-new-wearable-category-handwear 
+Incorrect: Props, Tiara
 
 What’s Decentraland’s first AI NPC called? 
 Aisha https://decentraland.org/blog/announcements/ai-npcs-herald-the-beginning-of-ai-in-decentraland 
+Incorrect: Eva, Maya
 
 What new feature was released this year combining wearables and coding? 
 Smart wearables https://decentraland.org/blog/announcements/smart-wearables-and-portable-experiences 
+Incorrect: CodeWears, Wearables 2.0
 
 What is the name of the new type of emotes which can have props and sound? 
 Emotes 2.0 https://decentraland.org/blog/announcements/expression-in-decentraland-gets-an-upgrade-emotes-2-0-are-live  
- */
+Incorrect: Smart Emote, Super Emote 
+*/
